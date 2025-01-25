d04ead89f8678fab11101ddb90df2cee7ad8ecee
Rohit2000-coder <142302975+Rohit2000-coder@users.noreply.github.com>
2025-01-25 06:35:45 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Web Devloper", "Blogger"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true
+})
